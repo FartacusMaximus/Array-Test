baf21c39db98db057c817c360915f6aad76b1536
william.arvidsson <william.arvidsson@elev.ga.lbs.se>
2018-10-23 09:32:06 +0200

    Array Test Start

---
+++ Assets/Script/Rave.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rave : MonoBehaviour
+{
+
+    public SpriteRenderer [] shipSprite;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for (loopar något)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            for(int i = 0; i < shipSprite.Length; i++)
+            {
+            shipSprite[i].color = Random.ColorHSV(0, 1, 0, 1, 1, 1);
+            }
+        }
+
+    }
+}
+
