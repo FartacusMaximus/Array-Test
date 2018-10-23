5387ae7796e99406588eaa043bca565612229c70
william.arvidsson <william.arvidsson@elev.ga.lbs.se>
2018-10-23 10:54:01 +0200

    Did stuff idk

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject spawnObejct;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            for (int i = 0; i < 100; i++)
+            Instantiate(spawnObejct, spawnPoints[Random.Range (0, spawnPoints.Length)].position,Quaternion.identity);
+        }
+    }
+}
